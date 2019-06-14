23a06af581eb9972165f355b532e46a830d88093
Patrick Brett <pkbrett37@gmail.com>
2019-06-14 23:21:35 +1000

    Refactor timer component with hooks

--- src/Timer.tsx
+++ src/Timer.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import TimerDone from './TimerDone'
 import { Command } from './types'
 
-const { Component } = React
+const { useEffect, useState } = React
 
 interface Props {
   duration: number;
@@ -11,96 +11,94 @@ interface Props {
   children: any;
 }
 
-interface State {}
+const Timer: (props: Props) => JSX.Element = (props) => {
+  const { duration, commandHistory, skew, children } = props
 
-class Timer extends Component<Props, State> {
-  interval: any = null
+  let interval = null;
 
-  componentDidUpdate = (): void => {
-    const { commandHistory } = { ...this.props }
+  const [currentTimestamp, setCurrentTimestamp] = useState(new Date().getTime())
 
-    clearInterval(this.interval)
+  const updateTime = () => {
+    setCurrentTimestamp(new Date().getTime())
+  }
+
+  useEffect(()=>{
+    updateTime()
+    clearInterval(interval)
 
     if (
       commandHistory.length &&
       commandHistory[commandHistory.length - 1].commandType === 'START'
     ) {
-      this.interval = setInterval(() => this.forceUpdate(), 100)
-    }
-  }
-
-  componentDidMount = (): void => {
-    const { commandHistory } = { ...this.props }
-
-    clearInterval(this.interval)
-
-    if (
-      commandHistory.length &&
-      commandHistory[commandHistory.length - 1].commandType === 'START'
-    ) {
-      this.interval = setInterval(() => this.forceUpdate(), 100)
-    }
-  }
-
-  render() {
-    const { duration, commandHistory, skew, active, children }: any = this.props
-
-    const currentTimestamp = new Date().getTime()
-
-    let elapsedDuration = 0
-    let sectionStart = 0
-    commandHistory.forEach((command: Command) => {
-      if (command.commandType === 'START') {
-        sectionStart = command.timestamp
-      } else if (command.commandType === 'PAUSE') {
-        elapsedDuration += command.timestamp - sectionStart
-        sectionStart = 0
-      }
-    })
-    if (sectionStart > 0) {
-      elapsedDuration += currentTimestamp - sectionStart
+      interval = setInterval(updateTime, 100)
     }
 
-    /*
-    skew to distortion
-    skew is between 0 and 100
-    skew of zero corresponds to distortion of 1 (no change)
-    skew of 100 corresponds to distortion of 0.6 (huge change)
+    return ()=>{
+      clearInterval(interval)
+    }
+  }, [commandHistory])
 
-    distortion = 1 - skew/100 * 0.4
-     */
+  let elapsedDuration = 0
+  let sectionStart = 0
 
-    const distortion: number = 1 - (skew / 100) * 0.4
+  commandHistory.forEach((command: Command) => {
+    if (command.commandType === 'START') {
+      sectionStart = command.timestamp
+    } else if (command.commandType === 'PAUSE') {
+      elapsedDuration += command.timestamp - sectionStart
+      sectionStart = 0
+    }
+  })
 
-    const durationMillis = duration * 1000
-
-    const finished = elapsedDuration >= durationMillis;
-
-    const modifiedTime = Math.ceil(
-      ((durationMillis - elapsedDuration) / durationMillis) ** distortion *
-        duration
-    )
-
-    const prependZero = (num: number) =>
-      num < 10 ? `0${String(num)}` : String(num)
-
-    const hours: string = String(Math.floor(modifiedTime / 3600))
-    const minutes: string = prependZero(Math.floor((modifiedTime % 3600) / 60))
-    const seconds: string = prependZero(Math.floor(modifiedTime % 60))
-
-    const timeDisplay = (
-      <div id="time-display">
-        {hours}:{minutes}:{seconds}
-      </div>
-    )
-
-    return (
-      <div id="timer" className={active ? 'large' : ''}>
-        {finished ? <TimerDone /> : timeDisplay}
-        {children}
-      </div>
-    )
+  if (sectionStart > 0) {
+    elapsedDuration += currentTimestamp - sectionStart
   }
+
+  if (elapsedDuration <= 0) {
+    elapsedDuration = 0
+  }
+
+  /*
+  skew to distortion
+  skew is between 0 and 100
+  skew of zero corresponds to distortion of 1 (no change)
+  skew of 100 corresponds to distortion of 0.6 (huge change)
+
+  distortion = 1 - skew/100 * 0.4
+   */
+
+  const distortion: number = 1 - (skew / 100) * 0.4
+
+  const durationMillis = duration * 1000
+
+  const finished = elapsedDuration >= durationMillis;
+
+  const modifiedTime = Math.ceil(
+    ((durationMillis - elapsedDuration) / durationMillis) ** distortion *
+      duration
+  )
+
+  const prependZero = (num: number) =>
+    num < 10 ? `0${String(num)}` : String(num)
+
+  const hours: string = String(Math.floor(modifiedTime / 3600))
+  const minutes: string = prependZero(Math.floor((modifiedTime % 3600) / 60))
+  const seconds: string = prependZero(Math.floor(modifiedTime % 60))
+
+  const active = false
+
+  const timeDisplay = (
+    <div id="time-display">
+      {hours}:{minutes}:{seconds}
+    </div>
+  )
+
+  return (
+    <div id="timer" className={active ? 'large' : ''}>
+      {finished ? <TimerDone /> : timeDisplay}
+      {children}
+    </div>
+  )
 }
 
 export default Timer
