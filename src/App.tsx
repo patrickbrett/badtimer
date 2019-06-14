317866bd444285ed4b200ce0d1ac7ec31b485909
Patrick Brett <pkbrett37@gmail.com>
2019-06-14 22:39:13 +1000

    Use hooks in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, Dispatch, SetStateAction, useEffect, useState } from 'react'
 import './App.css'
 import Timer from './Timer'
 import Button from './Button'
@@ -6,106 +6,73 @@ import About from './About'
 import TimeInput from './TimeInput'
 import { Command } from './types'
 
-interface Props {}
-
-interface State {
-  initialised: boolean;
-  duration: number;
-  commandHistory: Command[];
-  skew: number;
-  setup: Setup;
-  isShowingInfo: boolean;
-}
-
 interface Setup {
   hours: string;
   minutes: string;
   seconds: string;
 }
 
-type field = 'hours' | 'minutes' | 'seconds'
+type Field = 'hours' | 'minutes' | 'seconds'
 
-class App extends Component<Props, State> {
-  private readonly KEYCODE_ENTER: number
+const App = () => {
+  const SKEW_INIT = 50
 
-  constructor(props) {
-    super(props)
-
-    this.KEYCODE_ENTER = 13
-
-    const TIME_DISPLAY_DEFAULT = {
-      hours: '0',
-      minutes: '00',
-      seconds: '20',
-    }
-
-    const SKEW_INIT = 50
-
-    this.state = {
-      initialised: false,
-      duration: 0,
-      commandHistory: [],
-      skew: SKEW_INIT,
-      setup: TIME_DISPLAY_DEFAULT,
-      isShowingInfo: false,
-    }
+  const TIME_DISPLAY_DEFAULT = {
+    hours: '0',
+    minutes: '00',
+    seconds: '20',
   }
 
-  private readonly handleGlobalKeypress: any = e => {
-    if (e.keyCode === this.KEYCODE_ENTER) {
-      console.log('enter pressed')
-    }
-  }
+  const [initialised, setInitialised]: [boolean, Dispatch<SetStateAction<boolean>>] = useState(false)
+  const [duration, setDuration]: [number, Dispatch<SetStateAction<number>>] = useState(0)
+  const [commandHistory, setCommandHistory]: [Command[], Dispatch<SetStateAction<Command[]>>] = useState([])
+  const [skew, setSkew]: [number, Dispatch<SetStateAction<number>>] = useState(SKEW_INIT)
+  const [setup, setSetup]: [Setup, Dispatch<SetStateAction<Setup>>] = useState(TIME_DISPLAY_DEFAULT)
+  const [isShowingInfo, setIsShowingInfo]: [boolean, Dispatch<SetStateAction<boolean>>] = useState(false)
 
-  componentDidMount = () => {
-    document.addEventListener('keydown', this.handleGlobalKeypress)
-  }
+  const startTimer: () => void = () => {
+    setCommandHistory(prevCommandHistory => {
+      const newCommandHistory = [ ...prevCommandHistory ]
 
-  componentWillUnmount = () => {
-    document.removeEventListener('keydown', this.handleGlobalKeypress)
-  }
-
-  private readonly startTimer: () => void = () => {
-    this.setState(prevState => {
-      const { commandHistory } = {...prevState}
-
-      commandHistory.push({
+      newCommandHistory.push({
         commandType: 'START',
         timestamp: new Date().getTime()
       })
 
-      return { commandHistory }
+      return newCommandHistory
     })
   }
 
-  private readonly pauseTimer: () => void = () => {
-    this.setState(prevState => {
-      const { commandHistory } = {...prevState}
+  const pauseTimer: () => void = () => {
+    setCommandHistory(prevCommandHistory => {
+      const newCommandHistory = [ ...prevCommandHistory ]
 
-      commandHistory.push({
+      newCommandHistory.push({
         commandType: 'PAUSE',
         timestamp: new Date().getTime()
       })
 
-      return { commandHistory }
+      return newCommandHistory
     })
   }
 
-  private readonly stopTimer: () => void = () => {
-    this.setState({ commandHistory: [] })
+  const stopTimer: () => void = () => {
+    setCommandHistory([])
   }
 
-  private readonly editTimer: () => void = () => {
-    this.setState({ initialised: false })
+  const editTimer: () => void = () => {
+    setInitialised(false)
   }
 
-  private readonly updateSkew: (e) => void = e => {
+  const updateSkew: (e: any) => void = e => {
     const { value } = e.target
-    this.setState({ skew: value })
+    setSkew(value)
   }
 
-  private readonly updateField: (e, field: field) => void = (e, field) => {
+  const updateField: (e: any, field: Field) => void = (e, field) => {
     let { value } = e.target
+
+    console.log(value)
 
     if (value.match(/\d+/)) {
       value = value
@@ -116,140 +83,122 @@ class App extends Component<Props, State> {
       value = ''
     }
 
-    this.setState(prevState => {
-      const { setup } = prevState
-
-      setup[field] = value
-
-      return { setup }
+    setSetup(prevSetup => {
+      const newSetup = { ...prevSetup }
+      newSetup[field] = value
+      return newSetup
     })
   }
 
-  private readonly checkSubmit: (e) => void = e => {
-    if (e.key === 'Enter') {
-      this.finishInitialise()
-    }
-  }
-
-  private readonly finishInitialise: () => void = () => {
-    const { setup } = this.state
-
-    const duration: number =
+  const finishInitialise: () => void = () => {
+    const newDuration: number =
       Number(setup.hours) * 3600 +
       Number(setup.minutes) * 60 +
       Number(setup.seconds)
 
-    if (duration <= 0) {
-      this.setState({
-        setup: {
-          hours: '0',
-          minutes: '05',
-          seconds: '00',
-        },
+    if (newDuration <= 0) {
+      setSetup({
+        hours: '0',
+        minutes: '05',
+        seconds: '00',
       })
     } else {
-      this.setState({ initialised: true, duration })
+      setInitialised(true)
+      setDuration(newDuration)
     }
   }
 
-  private readonly toggleShowInfo: () => void = () => {
-    this.setState(prevState => ({ isShowingInfo: !prevState.isShowingInfo }))
+  const checkSubmit: (e) => void = e => {
+    if (e.key === 'Enter') {
+      finishInitialise()
+    }
   }
 
-  render() {
-    const {
-      initialised,
-      duration,
-      commandHistory,
-      skew,
-      setup,
-      isShowingInfo,
-    } = this.state
-
-    const active = commandHistory.length && commandHistory[commandHistory.length - 1].commandType === 'START'
-
-    const startButton = (
-      <Button name="start" onClick={this.startTimer} text="Start" />
-    )
-    const pauseButton = (
-      <Button name="pause" onClick={this.pauseTimer} text="Pause" />
-    )
-    const stopButton = (
-      <Button name="stop" onClick={this.stopTimer} text="Stop" />
-    )
-    const resetButton = (
-      <Button name="reset" onClick={this.stopTimer} text="Reset" />
-    )
-    const editButton = (
-      <Button name="edit" onClick={this.editTimer} text="Edit" />
-    )
-
-    const skewSlider = (
-      <div id="container-skew-slider">
-        not bad{' '}
-        <input
-          id="range-slider"
-          type="range"
-          min="0"
-          max="100"
-          value={skew}
-          onChange={this.updateSkew}
-        />{' '}
-        very bad
-      </div>
-    )
-
-    let buttonsContainer
-    buttonsContainer = (
-      <div id="container-buttons">
-        {active ? pauseButton : startButton}
-        {active ? stopButton : commandHistory.length ? resetButton : editButton}
-        {active ? null : skewSlider}
-      </div>
-    )
-
-    const timer = (
-      <Timer
-        duration={duration}
-        commandHistory={commandHistory}
-        skew={skew}
-        children={buttonsContainer}
-      />
-    )
-
-    const getTimeInput: (field) => JSX.Element = name => (
-      <TimeInput
-        name={name}
-        checkSubmit={this.checkSubmit}
-        setup={setup}
-        updateField={this.updateField}
-        isFocused={name === 'hours'}
-      />
-    )
-
-    const timerSetup = (
-      <div id="timer-setup">
-        {getTimeInput('hours')}:{getTimeInput('minutes')}:
-        {getTimeInput('seconds')}
-        <Button name="save" text="Save" onClick={this.finishInitialise} />
-      </div>
-    )
-
-    const containerTimer = (
-      <div id="container-timer">{initialised ? timer : timerSetup}</div>
-    )
-
-    const containerInfo = <About toggleShowInfo={this.toggleShowInfo} />
-
-    return (
-      <div id="App">
-        {isShowingInfo ? containerInfo : containerTimer}
-        <div id="about-button" onClick={this.toggleShowInfo}>
-          About
-        </div>
-      </div>
-    )
+  const toggleShowInfo: () => void = () => {
+    setIsShowingInfo(prevIsShowingInfo => !prevIsShowingInfo)
   }
+
+  const active = commandHistory.length && commandHistory[commandHistory.length - 1].commandType === 'START'
+
+  const startButton = (
+    <Button name="start" onClick={startTimer} text="Start" />
+  )
+  const pauseButton = (
+    <Button name="pause" onClick={pauseTimer} text="Pause" />
+  )
+  const stopButton = (
+    <Button name="stop" onClick={stopTimer} text="Stop" />
+  )
+  const resetButton = (
+    <Button name="reset" onClick={stopTimer} text="Reset" />
+  )
+  const editButton = (
+    <Button name="edit" onClick={editTimer} text="Edit" />
+  )
+
+  const skewSlider = (
+    <div id="container-skew-slider">
+      not bad{' '}
+      <input
+        id="range-slider"
+        type="range"
+        min="0"
+        max="100"
+        value={skew}
+        onChange={updateSkew}
+      />{' '}
+      very bad
+    </div>
+  )
+
+  const buttonsContainer = (
+    <div id="container-buttons">
+      {active ? pauseButton : startButton}
+      {active ? stopButton : commandHistory.length ? resetButton : editButton}
+      {active ? null : skewSlider}
+    </div>
+  )
+
+  const timer = (
+    <Timer
+      duration={duration}
+      commandHistory={commandHistory}
+      skew={skew}
+    >{buttonsContainer}</Timer>
+  )
+
+  const getTimeInput: (field: any) => JSX.Element = name => (
+    <TimeInput
+      name={name}
+      checkSubmit={checkSubmit}
+      setup={setup}
+      updateField={updateField}
+      isFocused={name === 'hours'}
+    />
+  )
+
+  const timerSetup = (
+    <div id="timer-setup">
+      {getTimeInput('hours')}:{getTimeInput('minutes')}:
+      {getTimeInput('seconds')}
+      <Button name="save" text="Save" onClick={finishInitialise} />
+    </div>
+  )
+
+  const containerTimer = (
+    <div id="container-timer">{initialised ? timer : timerSetup}</div>
+  )
+
+  const containerInfo = <About toggleShowInfo={toggleShowInfo} />
+
+  return (
+    <div id="App">
+      {isShowingInfo ? containerInfo : containerTimer}
+      <div id="about-button" onClick={toggleShowInfo}>
+        About
+      </div>
+    </div>
+  )
 }
 
 export default App
